ed6a665644d2e4eb31eb11f4f87bb495a635a263
Maanu449 <mansi10022000@gmail.com>
2018-06-12 14:47:21 +0530

    displaced test function and some small changes

--- model.py
+++ model.py
@@ -15,24 +15,12 @@ def model(X_train, Y_train, learning_rate=0.009, X=None, Y=None, weights=None, s
     init = tf.global_variables_initializer()
     sess.run(init)
     for epoch in range(num_epochs):
-        minibatch_cost = 0.
-        num_minibatches = int(m / minibatch_size)
+        # minibatch_cost = 0.
+        # num_minibatches = int(m / minibatch_size)
         minibatches = random_mini_batches(X_train, Y_train, minibatch_size)
         for (minibatch_X, minibatch_Y) in minibatches:
             _, temp_cost = sess.run([optimizer, cost], feed_dict={Y:minibatch_Y, vgg.imgs:minibatch_X})
-            print(temp_cost)
-
-def test(X_test, Y_test, sess):
-        X = tf.placeholder(dtype=tf.float32, shape= [None, 224,224,3])
-        vgg = vgg16(X, 'init_weights.npz', sess)
-        predict_op = tf.argmax(vgg.fc4l, 1)
-        correct_prediction = tf.equal(predict_op, tf.argmax(Y, 1))
-        accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-        # train_accuracy = accuracy.eval({vgg.imgs: X_train, Y: Y_train})
-        test_accuracy = sess.run(accuracy, feed_dict={vgg.imgs: X_test, Y: Y_test})
-        # print("Train Accuracy:", train_accuracy)
-        print("Test Accuracy:", test_accuracy)
-                
+            print(temp_cost)            
 
 sess = tf.Session()
 X = tf.placeholder(tf.float32, [None, 224, 224, 3])
@@ -44,7 +32,7 @@ count = -1
 for y in Y_train:
     count += 1
     Y_train[count] = [0,1] if Y_train[count][0]<=Y_train[count][1] else [1,0]
-img1 = imread('img.png', mode='RGB')
-img1 = imresize(img1, (224, 224))
+#img1 = imread('img.png', mode='RGB')
+#img1 = imresize(img1, (224, 224))
 test(X_train, Y_train, sess)
 # model(X_train, Y_train, 0.009, X, Y, 'init_weights.npz', sess)
--- utils.py
+++ utils.py
@@ -28,3 +28,16 @@ def random_mini_batches(X, Y, mini_batch_size):
         mini_batch = (mini_batch_X, mini_batch_Y)
         mini_batches.append(mini_batch)
     return mini_batches
+
+def test(X_test, Y_test, sess):
+        X = tf.placeholder(dtype=tf.float32, shape=[None, 224,224,3])
+        Y = tf.placeholder(dtype=tf.float32, shape=[None, 2])
+        vgg = vgg16(X, 'init_weights.npz', sess)
+        predict_op = tf.argmax(vgg.fc4l, 1)
+        correct_prediction = tf.equal(predict_op, tf.argmax(Y, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+        # train_accuracy = accuracy.eval({vgg.imgs: X_train, Y: Y_train})
+        test_accuracy = sess.run(accuracy, feed_dict={vgg.imgs: X_test, Y: Y_test})
+        # print("Train Accuracy:", train_accuracy)
+        print("Test Accuracy:", test_accuracy)
+   
