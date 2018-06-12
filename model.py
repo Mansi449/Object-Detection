0af872a2bfb7e8bff4e6a8bb7cf6040d64d3f1cb
Harsh Kumar Bhartiya <ilikegamess@gmail.com>
2018-06-12 14:08:27 +0530

    Working model

--- model.py
+++ model.py
@@ -3,34 +3,9 @@ import tensorflow as tf
 from scipy.misc import imread, imresize
 from vgg16 import vgg16
 import math
+from utils import *
 
-def random_mini_batches(X, Y, mini_batch_size = 64, seed = 0):
-    m = X.shape[0]                  # number of training examples
-    mini_batches = []
-    np.random.seed(seed)
-    
-    # Step 1: Shuffle (X, Y)
-    permutation = list(np.random.permutation(m))
-    shuffled_X = X[permutation,:,:,:]
-    shuffled_Y = Y[permutation,:]
-
-    # Step 2: Partition (shuffled_X, shuffled_Y). Minus the end case.
-    num_complete_minibatches = math.floor(m/mini_batch_size) # number of mini batches of size mini_batch_size in your partitionning
-    for k in range(0, num_complete_minibatches):
-        mini_batch_X = shuffled_X[k * mini_batch_size : k * mini_batch_size + mini_batch_size,:,:,:]
-        mini_batch_Y = shuffled_Y[k * mini_batch_size : k * mini_batch_size + mini_batch_size,:]
-        mini_batch = (mini_batch_X, mini_batch_Y)
-        mini_batches.append(mini_batch)
-    
-    # Handling the end case (last mini-batch < mini_batch_size)
-    if m % mini_batch_size != 0:
-        mini_batch_X = shuffled_X[num_complete_minibatches * mini_batch_size : m,:,:,:]
-        mini_batch_Y = shuffled_Y[num_complete_minibatches * mini_batch_size : m,:]
-        mini_batch = (mini_batch_X, mini_batch_Y)
-        mini_batches.append(mini_batch)
-    return mini_batches
-
-def model(X_train, Y_train, learning_rate=0.009, X=None, Y=None, weights=None, sess=None, num_epochs=100, minibatch_size=64):
+def model(X_train, Y_train, learning_rate=0.009, X=None, Y=None, weights=None, sess=None, num_epochs=3, minibatch_size=5):
     m = X_train.shape[0]
     if weights is not None and sess is not None:
         vgg = vgg16(X, weights, sess)
@@ -45,18 +20,31 @@ def model(X_train, Y_train, learning_rate=0.009, X=None, Y=None, weights=None, s
         minibatches = random_mini_batches(X_train, Y_train, minibatch_size)
         for (minibatch_X, minibatch_Y) in minibatches:
             _, temp_cost = sess.run([optimizer, cost], feed_dict={Y:minibatch_Y, vgg.imgs:minibatch_X})
-    # print(sess.run(cost, feed_dict={vgg.imgs: [img1]}))
+            print(temp_cost)
+
+def test(X_test, Y_test, sess):
+        X = tf.placeholder(dtype=tf.float32, shape= [None, 224,224,3])
+        vgg = vgg16(X, 'init_weights.npz', sess)
+        predict_op = tf.argmax(vgg.fc4l, 1)
+        correct_prediction = tf.equal(predict_op, tf.argmax(Y, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+        # train_accuracy = accuracy.eval({vgg.imgs: X_train, Y: Y_train})
+        test_accuracy = sess.run(accuracy, feed_dict={vgg.imgs: X_test, Y: Y_test})
+        # print("Train Accuracy:", train_accuracy)
+        print("Test Accuracy:", test_accuracy)
+                
 
 sess = tf.Session()
 X = tf.placeholder(tf.float32, [None, 224, 224, 3])
 Y = tf.placeholder(tf.float32, [None, 2])
 # Y = tf.constant([0,1], dtype = tf.float32)
-X_train = np.random.randint(225,size=(10,224,224,3))
-Y_train = np.random.randint(10,size=(10,2))
+X_train = np.random.randint(225,size=(11,224,224,3))
+Y_train = np.random.randint(10,size=(11,2))
 count = -1
 for y in Y_train:
     count += 1
     Y_train[count] = [0,1] if Y_train[count][0]<=Y_train[count][1] else [1,0]
 img1 = imread('img.png', mode='RGB')
 img1 = imresize(img1, (224, 224))
-model(X_train, Y_train, 0.009, X, Y, 'init_weights.npz', sess)
+test(X_train, Y_train, sess)
+# model(X_train, Y_train, 0.009, X, Y, 'init_weights.npz', sess)
---
+++ utils.py
@@ -0,0 +1,30 @@
+import numpy as np
+import tensorflow as tf
+from scipy.misc import imread, imresize
+from vgg16 import vgg16
+import math
+
+def random_mini_batches(X, Y, mini_batch_size):
+    m = X.shape[0]                  # number of training examples
+    mini_batches = []
+    
+    # Step 1: Shuffle (X, Y)
+    permutation = list(np.random.permutation(m))
+    shuffled_X = X[permutation,:,:,:]
+    shuffled_Y = Y[permutation,:]
+
+    # Step 2: Partition (shuffled_X, shuffled_Y). Minus the end case.
+    num_complete_minibatches = math.floor(m/mini_batch_size) # number of mini batches of size mini_batch_size in your partitionning
+    for k in range(0, num_complete_minibatches):
+        mini_batch_X = shuffled_X[k * mini_batch_size : k * mini_batch_size + mini_batch_size,:,:,:]
+        mini_batch_Y = shuffled_Y[k * mini_batch_size : k * mini_batch_size + mini_batch_size,:]
+        mini_batch = (mini_batch_X, mini_batch_Y)
+        mini_batches.append(mini_batch)
+    
+    # Handling the end case (last mini-batch < mini_batch_size)
+    if m % mini_batch_size != 0:
+        mini_batch_X = shuffled_X[num_complete_minibatches * mini_batch_size : m,:,:,:]
+        mini_batch_Y = shuffled_Y[num_complete_minibatches * mini_batch_size : m,:]
+        mini_batch = (mini_batch_X, mini_batch_Y)
+        mini_batches.append(mini_batch)
+    return mini_batches
